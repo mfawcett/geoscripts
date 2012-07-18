0a399a0f27e2383792f914a86cd2b734672ecd52
Michael Fawcett <michael.fawcett@lmnsolutions.com>
2012-07-18 16:28:05 -0400

    Refactored into separate scripts folder for all future geoscripts. Added DistanceBearing skeleton code.

---
+++ scripts/DistanceBearing.js
@@ -0,0 +1,35 @@
+var gs = require('../lib/geoscript');
+
+exports.distanceBearing = function () {
+	// Hard-coded inputs.  These should be passed in as generically as possible
+	var postgis = new gs.workspace.PostGIS({
+		database:	"GeoScript_Test",
+		host:		"192.168.10.140",
+		port:		5432,
+		schema:		"public",
+		user:		"postgres",
+		password:	"p0stGISAdm!n"	
+	});
+	var features = postgis.get("polygons").query(new gs.filter.Filter("CONTAINS(buffer(POINT(-122.7668 42.4979), 0.01), centroid(geom))"));
+//			"DWITHIN(centroid(geom), POINT(-122.7668 42.4979), 700, meters)"));
+	// End hard-coded inputs
+
+	var i = 0;
+	features.forEach(function (f) {
+		i = i + 1;
+		var calc  = new Packages.org.geotools.referencing.GeodeticCalculator();
+		calc.setStartingGeographicPoint(-122.7668, 42.4979);
+		
+		var p = f.geometry.centroid;
+		calc.setDestinationGeographicPoint(p.x, p.y);
+		
+		var bearing = calc.getAzimuth();
+		print("bearing: " + bearing);
+		
+		var distance = calc.getOrthodromicDistance();
+		print("distance: " + distance);
+	});
+
+	print("feature count: " + i);
+	postgis.close();
+}
