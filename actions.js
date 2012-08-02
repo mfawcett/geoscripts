0732d3c5ea0439808a32d60bdb9200c7f4959319
Syrus <syrus.mesdaghi@lmnsolutions.com>
2012-08-02 15:19:19 -0400

    1) parameter format changed to be a bit simplified. x, y coming in separately. 2) Also support for json and url encoded params. 3) requests that are preflight with OPTIONS are handled so that the POST that follows works as expected.

--- actions.js
+++ actions.js
@@ -5,6 +5,27 @@ var httpclient = require('ringo/httpclient');
 var gs = require('./lib/geoscript');
 var db = require('./scripts/DistanceBearing');
 
+// character codes used for slicing and decoding
+var SPACE     = " ".charCodeAt(0);
+var PERCENT   = "%".charCodeAt(0);
+var AMPERSAND = "&".charCodeAt(0);
+var PLUS      = "+".charCodeAt(0);
+var EQUALS    = "=".charCodeAt(0);
+
+// character codes used for hex decoding
+var CHAR_0 = "0".charCodeAt(0);
+var CHAR_9 = "9".charCodeAt(0);
+var CHAR_A = "A".charCodeAt(0);
+var CHAR_F = "F".charCodeAt(0);
+var CHAR_a = "a".charCodeAt(0);
+var CHAR_f = "f".charCodeAt(0);
+
+// used for multipart parsing
+var HYPHEN  = "-".charCodeAt(0);
+var CR = "\r".charCodeAt(0);
+var CRLF = new ByteString("\r\n", "ASCII");
+var EMPTY_LINE = new ByteString("\r\n\r\n", "ASCII");
+
 exports.index = function (req) {
     var template = getResource("./templates/index.html").content;
     return response.html(
@@ -14,9 +35,9 @@ exports.index = function (req) {
     );
 };
 
-function makeBBox(loc, radius_m) {
+function makeBBox(x, y, radius_m) {
 	var calc  = new Packages.org.geotools.referencing.GeodeticCalculator();
-	calc.setStartingGeographicPoint(loc.x, loc.y);
+	calc.setStartingGeographicPoint(x, y);
 	
 	calc.setDirection(0, radius_m);
 	var north = calc.getDestinationGeographicPoint();
@@ -36,23 +57,113 @@ function makeBBox(loc, radius_m) {
 	};
 }
 
-exports.wps = function (req) {
-	var params = utils.parseParameters(req.input.read());
-	var bbox = makeBBox(params.location, params.radius);
-
-	var request = "?request=GetFeature&version=1.0.0&typeName=" + params.typeName + "&BBOX=" +
-		bbox.lowerleft.x + "," + bbox.lowerleft.y + "," + bbox.upperright.x + "," + bbox.upperright.y + ",EPSG:4326&outputFormat=JSON";
-
-	var exchange = httpclient.get(params.wfs + request);
-
-    var obj = JSON.parse(exchange.content);
-    if (obj.type != "FeatureCollection") {
-        throw new Error("Invalid GeoJSON type - " + obj.type);
+function isEmpty(obj) {
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            return false;
     }
-    
-    obj.features.forEach(function(f) {
-    	f.geometry = gs.geom.create(f.geometry);
-    });
-    
-	return response.json(db.distanceBearing(params, obj.features));
-};
+
+    return true;
+}
+
+exports.wps = function (req) {
+	
+	// if the request received is of method "OPTIONS", respond with okay so that the POST can follow as expected 
+	if(req.method === "OPTIONS") {
+		return {
+	        status: 200,
+	        headers: {"Content-Type": "text/plain",
+	        	"Access-Control-Allow-Origin": "*",
+	        	"Access-Control-Allow-Methods": "POST, GET",
+	        	"Access-Control-Allow-Headers": "x-requested-with,Content-Type"},
+	        body: ["reponse to preflight"]
+	    };
+	}
+	
+	
+	var input = req.input.read();
+	var params = null;
+	
+	// handle json and application/x-www-form-urlencoded input
+	if (req.headers["content-type"] === "application/json") {
+		var inputString =  decodeToString(input);
+		params = eval('(' + inputString + ')');
+	} else {
+		params = utils.parseParameters(input);		
+	}
+	
+	print("params {");
+	for (var key in params) {
+		print("  " + key + " = " + params[key]);
+	}
+	print("} // params");
+	
+	
+	if (params && params.x && params.y && params.radius && params.wfs && params.typeName) {
+
+	    var resp = null;
+		
+		var bbox = makeBBox(params.x, params.y, params.radius);
+
+		var request = "?request=GetFeature&version=1.0.0&typeName=" + params.typeName + "&BBOX=" +
+			bbox.lowerleft.x + "," + bbox.lowerleft.y + "," + bbox.upperright.x + "," + bbox.upperright.y + ",EPSG:4326&outputFormat=JSON";
+
+		var exchange = httpclient.get(params.wfs + request);
+
+	    var obj = JSON.parse(exchange.content);
+	    if (obj.type != "FeatureCollection") {
+	        throw new Error("Invalid GeoJSON type - " + obj.type);
+	    }
+	    
+	    obj.features.forEach(function(f) {
+	    	f.geometry = gs.geom.create(f.geometry);
+	    });
+	    
+	    resp = response.json(db.distanceBearing(params, obj.features));
+	} else {
+		print("WARNING: Parameters not specified as expected.");
+		resp = {
+	        status: 500,
+	        headers: {"Content-Type": "text/plain",
+	        	"Access-Control-Allow-Origin": "*",
+	        	"Access-Control-Allow-Methods": "POST, GET",
+	        	"Access-Control-Allow-Headers": "x-requested-with,Content-Type"},
+	        body: ["Parameters not specified as expected."]
+	    };
+	}
+
+    resp.headers['Access-Control-Allow-Origin'] = '*';
+    return resp;
+ };
+
+// convert + to spaces, decode %ff hex sequences,
+// then decode to string using the specified encoding.
+function decodeToString(bytes, encoding) {
+    var k = 0;
+    while((k = bytes.indexOf(PLUS, k)) > -1) {
+        bytes[k++] = SPACE;
+    }
+    var i, j = 0;
+    while((i = bytes.indexOf(PERCENT, j)) > -1) {
+        j = i;
+        while (bytes[i] == PERCENT && i++ <= bytes.length - 3) {
+            bytes[j++] = (convertHexDigit(bytes[i++]) << 4)
+                        + convertHexDigit(bytes[i++]);
+        }
+        if (i < bytes.length) {
+            bytes.copy(i, bytes.length, bytes, j);
+        }
+        bytes.length -= i - j;
+    }
+    return bytes.decodeToString(encoding);
+}
+
+function convertHexDigit(byte) {
+    if (byte >= CHAR_0 && byte <= CHAR_9)
+        return byte - CHAR_0;
+    if (byte >= CHAR_a && byte <= CHAR_f)
+        return byte - CHAR_a + 10;
+    if (byte >= CHAR_A && byte <= CHAR_F)
+        return byte - CHAR_A + 10;
+    return 0;
+}
